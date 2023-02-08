eb3c5fa06bb2071a38713db5245806151289897b
Kennedy Tedesco <kennedyt.tw@gmail.com>
2023-02-08 09:43:15 -0300

    new: source content is now optional

---
+++ src/hash.rs
@@ -0,0 +1,22 @@
+use std::hash::Hasher;
+
+pub trait ContentHasher: Send + Sync {
+    fn hash(&self, content: &str) -> u64;
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct FxHasher;
+
+impl FxHasher {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl ContentHasher for FxHasher {
+    fn hash(&self, content: &str) -> u64 {
+        let mut hasher = rustc_hash::FxHasher::default();
+        hasher.write(content.as_bytes());
+        hasher.finish()
+    }
+}
--- src/source.rs
+++ src/source.rs
@@ -1,20 +1,36 @@
+use std::fs;
+use std::path::PathBuf;
+
+use crate::hash::ContentHasher;
+use crate::hash::FxHasher;
+
 pub const DEFAULT_NAME: &str = "<unknown>";
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum SourceKind {
     /// A definition is a piece of code that is not executed, but can be used
-    /// to define foriegn symbols ( e.g from PHP ).
+    /// to define foreign symbols ( e.g from PHP ).
     Definition,
 
     /// A script is a piece of code that is executed.
     Script,
 }
 
+pub trait SourceTrait {
+    /// Reads the source content.
+    fn content(&mut self) -> std::io::Result<String>;
+
+    /// Returns the source content hash.
+    fn hash(&mut self) -> std::io::Result<u64>;
+}
+
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Source {
     pub kind: SourceKind,
+    pub root: Option<PathBuf>,
     pub origin: Option<String>,
-    pub content: String,
+    pub content: Option<String>,
+    hasher: FxHasher,
 }
 
 /// A source.
@@ -27,40 +43,24 @@ pub struct Source {
 /// use ara_source::source::Source;
 /// use ara_source::source::SourceKind;
 ///
-/// let source = Source::new(SourceKind::Script, "main.ara", "function main(): void {}");
+/// let source = Source::new(SourceKind::Script, "/Documents/Project", "src/main.ara");
 ///
 /// assert_eq!(source.kind, SourceKind::Script);
-/// assert_eq!(source.origin, Some("main.ara".to_string()));
-/// assert_eq!(source.content, "function main(): void {}");
+/// assert_eq!(source.origin, Some("src/main.ara".to_string()));
+/// assert_eq!(source.root, Some("/Documents/Project".into()));
+/// assert_eq!(source.content, None);
 ///
-/// assert_eq!(source.name(), "main.ara");
+/// assert_eq!(source.name(), "src/main.ara");
 /// ```
 impl Source {
-    /// Create a new source with the given content.
-    ///
-    /// Example:
-    ///
-    /// ```rust
-    /// use ara_source::source::Source;
-    /// use ara_source::source::SourceKind;
-    ///
-    /// let source = Source::inline(SourceKind::Definition, "function main(): void {}");
-    ///
-    /// assert_eq!(source.kind, SourceKind::Definition);
-    /// assert_eq!(source.origin, None);
-    /// assert_eq!(source.content, "function main(): void {}");
-    ///
-    /// assert_eq!(source.name(), "<unknown>");
-    /// ```
-    pub fn new<O: Into<String>, C: Into<String>>(
-        kind: SourceKind,
-        origin: O,
-        content: C,
-    ) -> Source {
+    /// Create a new source with the given origin.
+    pub fn new<O: Into<String>, R: Into<PathBuf>>(kind: SourceKind, root: R, origin: O) -> Source {
         Source {
             kind,
+            root: Some(root.into()),
             origin: Some(origin.into()),
-            content: content.into(),
+            content: None,
+            hasher: FxHasher::new(),
         }
     }
 
@@ -75,14 +75,19 @@ impl Source {
     /// let source = Source::inline(SourceKind::Definition, "function main(): void {}");
     ///
     /// assert_eq!(source.kind, SourceKind::Definition);
+    /// assert_eq!(source.root, None);
     /// assert_eq!(source.origin, None);
-    /// assert_eq!(source.content, "function main(): void {}");
+    /// assert_eq!(source.content, Some("function main(): void {}".to_string()));
+    ///
+    /// assert_eq!(source.name(), "<unknown>");
     /// ```
     pub fn inline<C: Into<String>>(kind: SourceKind, content: C) -> Source {
         Source {
             kind,
+            root: None,
             origin: None,
-            content: content.into(),
+            content: Some(content.into()),
+            hasher: FxHasher::new(),
         }
     }
 
@@ -97,16 +102,53 @@ impl Source {
     /// use ara_source::source::Source;
     /// use ara_source::source::SourceKind;
     ///
-    /// let source = Source::new(SourceKind::Definition, "main.ara", "function main(): void {}");
-    /// assert_eq!(source.name(), "main.ara");
+    /// let source = Source::new(SourceKind::Definition, "/Documents/Project", "src/Foo/main.ara");
+    /// assert_eq!(source.name(), "src/Foo/main.ara");
     ///
     /// let source = Source::inline(SourceKind::Definition, "function main(): void {}");
     /// assert_eq!(source.name(), "<unknown>");
     /// ```
     pub fn name(&self) -> &str {
-        match self.origin {
-            Some(ref origin) => origin,
+        match &self.origin {
+            Some(origin) => origin,
             None => DEFAULT_NAME,
         }
+    }
+
+    /// Returns the complete path of the source.
+    ///
+    /// Example:
+    ///
+    /// ```rust
+    /// use ara_source::source::Source;
+    /// use ara_source::source::SourceKind;
+    ///
+    /// let source = Source::new(SourceKind::Definition, "/Documents/Project", "src/Foo/main.ara");
+    /// assert_eq!(source.source_path(), Some("/Documents/Project/src/Foo/main.ara".into()));
+    /// ```
+    pub fn source_path(&self) -> Option<PathBuf> {
+        self.root
+            .as_ref()
+            .map(|root| root.join(self.origin.as_ref().unwrap()))
+    }
+}
+
+impl SourceTrait for Source {
+    fn content(&mut self) -> std::io::Result<String> {
+        let path = self
+            .source_path()
+            .expect("Both root and origin must be present in order to read the source content");
+
+        fs::read_to_string(path)
+    }
+
+    fn hash(&mut self) -> std::io::Result<u64> {
+        if self.content.is_some() {
+            return Ok(self.hasher.hash(self.content.as_ref().unwrap()));
+        }
+
+        let content = self.content()?;
+
+        Ok(self.hasher.hash(&content))
     }
 }
